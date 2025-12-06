c242f5cf6d2790c4da463059cab1fbbae06ae61b
sudarshan24-10 <sudarshangampa24@gmail.com>
2025-12-06 19:03:26 +0530

    update

--- server/settings.py
+++ server/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Settings:
     def __init__(self):
-        self.MONGO_URI: str = os.getenv("MONGO_URI", "mongodb+srv://sudarshan2:0250@cluster0.zvvo4kp.mongodb.net/?retryWrites=true&w=majority")
+        self.MONGO_URI: str = os.getenv("MONGO_URI", "")
         self.DB_NAME: str = os.getenv("DB_NAME", "core_db")
         self.CORS_ORIGINS: str = os.getenv("CORS_ORIGINS", "*")
         self.JWT_SECRET: str = os.getenv("JWT_SECRET", "defaultsecret")
